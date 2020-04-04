bf19f766c9f57ae549c45b20c0b92dbe549137dc
Ahmad Elawady <ai.elawady@gmail.com>
2020-04-04 15:23:46 +0200

    Initial commit

---
+++ visers.py
@@ -0,0 +1,15 @@
+import cv2
+
+
+def visualize_faces(img):
+	img [...,1:] = 0
+	return img
+
+def visualize_action(img):
+	img[...,0] = 0
+	img[...,2] = 0
+	return img
+
+def visualize_pose(img):
+	img[...,:-1] = 0
+	return img
