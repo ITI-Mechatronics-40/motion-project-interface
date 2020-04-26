251b008e123dfb7e86c496d0a8f48c1f57f9106e
Samuel Youssef <samuelm.youssef@gmail.com>
2020-04-26 11:47:42 +0200

    Implementing visualize_pose function

--- visualizers.py
+++ visualizers.py
@@ -1,13 +1,33 @@
+import requests
+import json
+import base64
+import numpy as np
+import cv2
+
+
+def encode_img(image):
+    _, buffer = cv2.imencode('.jpg', image)
+    enc_buff = base64.b64encode(buffer)
+    return str(enc_buff, 'utf-8')
+
 
 def visualize_faces(img):
 	img [...,1:] = 0
 	return img
+
 
 def visualize_activity(img):
 	img[...,0] = 0
 	img[...,2] = 0
 	return img
 
+
 def visualize_pose(img):
-	img[...,:-1] = 0
-	return img
+    url = "http://0.0.0.0:5001/"
+    headers = {'Content-Type': 'application/json'}
+    image_req = json.dumps({'img': str(encode_img(img))})
+    response = requests.request("GET", url=url+'analyse_image', headers=headers, data=image_req)
+    img = json.loads(response.content)['data']
+    img = np.array(img, dtype=np.uint8)
+
+    return img
